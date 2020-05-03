41570e0aedf7ae5b0eb9c4558cdeee5591829965
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-05-04 01:39:21 +0530

    email verif

--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ var UserSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
     email:  {type: String, unique: true, required: true},
     password: String,
+    isVerified: { type: Boolean,default: false},
+    verificationToken: String,
+    verificationTokenExpires: Date,
     resetPasswordToken: String,
     resetPasswordExpires: Date
 });
